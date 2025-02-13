9a460ab50f9a553f3e7dd8e2ce70573b4b2167f7
Mahortin <nitramwolfrum@web.de>
2025-02-13 14:34:37 +0100

    replace vue icon

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/phoenix.png">
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <AttributePage></AttributePage>
 </template>
